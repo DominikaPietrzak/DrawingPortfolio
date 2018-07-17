fb45749ac9a2062db3e62de6d77be06a72bb2842
DominikaPieRep <dompie6@st.amu.edu.pl>
2018-07-17 14:24:37 +0200

    add comic pages views

--- app/controllers/comic_pages_controller.rb
+++ app/controllers/comic_pages_controller.rb
@@ -1,2 +1,21 @@
 class ComicPagesController < ApplicationController
+
+  def index
+    @pages = ComicPage.all
+  end
+
+  def new
+    @page = ComicPage.new(params)
+  end
+
+  def edit
+
+  end
+
+  def show
+    @page = ComicPage.find(prams[:id])
+  end
+
+  def create
+  end
 end
