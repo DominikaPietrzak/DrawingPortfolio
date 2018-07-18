9cdb4b64adb8217213ec910bc56a3726c92bbba1
DominikaPieRep <dompie6@st.amu.edu.pl>
2018-07-18 11:32:52 +0200

    redactor new chapter , add create function, and chapter params

--- app/controllers/comic_chapters_controller.rb
+++ app/controllers/comic_chapters_controller.rb
@@ -1,17 +1,27 @@
 class ComicChaptersController < ApplicationController
 
+  before_action :authenticate_user!
+
   def index
     @chapters = ComicChapter.all
   end
 
   def new
-    @comic_book = ComicBook.find_by(params[:id])
-    @new_chapter = ComicChapter.new(comic_book_id: @comic_book)
+    @comic_chapter = current_user.comic_chapters.build
   end
 
   def show
     @chapter = ComicChapter.find(params[:id])
-
   end
 
+  def create
+    @comic_chapter = current_user.comic_chapters.build(chapter_params)
+    @comic_chapter.save
+  end
+
+  private
+
+  def chapter_params
+    params.require(:comic_chapter).permit(:title, :number, :comic_book_id)
+  end
 end
