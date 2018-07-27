773ca9749274b0683e5fdd262100c2faed6f0324
DominikaPieRep <dompie6@st.amu.edu.pl>
2018-07-27 12:31:29 +0200

    remove space beetwen images

--- app/models/comic_book.rb
+++ app/models/comic_book.rb
@@ -1,7 +1,8 @@
 class ComicBook < ApplicationRecord
+
   has_many :comic_chapters
   belongs_to :user
-  has_attached_file :comiccover, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :comiccover, styles: { medium: "500x500>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :comiccover, content_type: /\Aimage\/.*\z/
 
 
