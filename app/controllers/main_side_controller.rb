e1271286a9f83755c35f5fc238ad135afc7040a1
DominikaPieRep <dompie6@st.amu.edu.pl>
2018-07-27 10:23:09 +0200

    add styles for main page

--- app/controllers/main_side_controller.rb
+++ app/controllers/main_side_controller.rb
@@ -1,10 +1,24 @@
 class MainSideController < ApplicationController
 
   def index
-    @pages = ComicPage.limit(12)
-    @comics = ComicBook.limit(12)
-    @chapters = ComicChapter.limit(12)
-    @arts = Art.limit(10)
+
+    @pages = ComicPage.limit(3)
+    @pages2 = ComicPage.limit(3)
+    @pages3 = ComicPage.limit(3)
+
+    @comics = ComicBook.limit(3)
+    @comics2 = ComicBook.limit(3)
+    @comics3 = ComicBook.limit(3)
+
+    @chapters = ComicChapter.limit(3)
+    @chapters2 =ComicChapter.limit(3)
+    @chapters3 = ComicChapter.limit(3)
+
+
+    @arts = Art.limit(3)
+    @arts2 = Art.limit(3)
+    @arts3 = Art.limit(3)
+
   end
 
 end
