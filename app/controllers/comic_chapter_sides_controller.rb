4ad89514422c663d8af931ab72542a74c5c080b8
DominikaPieRep <dompie6@st.amu.edu.pl>
2018-08-01 12:56:27 +0200

    add link to images

--- app/controllers/comic_chapter_sides_controller.rb
+++ app/controllers/comic_chapter_sides_controller.rb
@@ -2,14 +2,24 @@ class ComicChapterSidesController < ApplicationController
 
   def index
     @page = ComicChapter.find(params[:id])
-    
   end
 
   def show
-    @chapter = ComicChapter.find(params[:id])
 
-    @pages = ComicPage.where(comic_chapter_id:@chapter.id )
-    @firstPage = @pages.first
+    if params[:id]
+      @chapter = ComicChapter.find(params[:id])
+      @pages = ComicPage.where(comic_chapter_id:@chapter.id )
+      @firstPage = @pages.first
+    end
+
+    if params[:format]
+      chap_id = params[:format]
+      @chapter = ComicChapter.find_by(id: chap_id)
+      @pages = ComicPage.where(comic_chapter_id: @chapter.id )
+      @firstPage = @pages.first
+
+    end
+
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,4 +17,5 @@ Rails.application.routes.draw do
   match '/comics/:id/:title' => 'comic_sides#show', :as => :comic_sides_with_title, via: [:get, :post]
   match '/chapters/:id/:title' => 'comic_chapter_sides#show', :as => :comic_chapter_sides_with_title, via: [:get, :post]
   match '/pages/:comic_chapter_id/:number' => 'comic_page_sides#show', :as => :comic_page_side, via: [:get, :post]
+  get 'comic_chapter_sides', to: 'comic_chapter_sides#show'
 end
